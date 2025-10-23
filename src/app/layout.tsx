4f1da26c221eca6fba888e258c292beab95e3657
jls846 <jlussanchez123@gmail.com>
2025-10-23 15:35:17 -0600

    modificacion a fonts en archivo globales

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,18 @@
 import Header from "../components/header";
+import { Inter, Montserrat } from "next/font/google";
+import "../app/styles/base/_globales.scss"; // si tienes estilos globales
 
-
+// Carga optimizada de fuentes
+const inter = Inter({ subsets: ["latin"], weight: ["400", "700"], variable: "--font-inter" });
+const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "700"], variable: "--font-montserrat" });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="es">
+    <html lang="es" className={`${inter.variable} ${montserrat.variable}`}>
       <body>
         <Header />
-       {children}
+        {children}
       </body>
     </html>
-  )
+  );
 }
