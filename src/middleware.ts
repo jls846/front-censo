21b7cfac6adb5ebcb45b87f014dc76e6ae35fba9
IO <320154041@pcpuma.acatlan.unam.mx>
2025-11-12 19:25:13 -0600

    delete redirect to /

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,6 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
-  if (!token) {
-    console.log("No hay token, redirigiendo a la página de inicio de sesión");
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
   return NextResponse.next();
 }
 
